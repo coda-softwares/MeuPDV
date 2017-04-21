b8f8c4c5bb57393a62c984d7b58f78c076441a9c
Samuel Henrique <samosaara@gmail.com>
2017-04-21 13:42:07 -0300

    Initial commit

---
+++ app/src/main/java/etec/coda_softwares/meupdv/TelaInicial.java
@@ -0,0 +1,13 @@
+package etec.coda_softwares.meupdv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TelaInicial extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
