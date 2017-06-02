5748f80e81bc4c415198d904a396b7b4a90f0f43
jeffbustercase <jefferson_bomfim@outlook.com>
2017-06-02 15:51:24 +0000

    Adicionei TODO's e base fix updateQuatidade(Produto)

--- app/src/main/java/etec/coda_softwares/meupdv/AtualizarEstoque.java
+++ app/src/main/java/etec/coda_softwares/meupdv/AtualizarEstoque.java
@@ -21,6 +21,23 @@ public class AtualizarEstoque extends AppCompatActivity
     public ListView listaProdutos;
 
     @Override
+    protected boolean onCreateOptionsMenu(Bundle savedInstanceState){
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.menu_produtos, menu);
+        // TODO: Criar novo (XML CONFIRMA ICON)
+        MenuItem item = menu.findItem(R.id.menu_atualizar_confirma);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // TODO: Salvar modificações
+                // ~ for produtos from listaProdutos
+                // - Usar updateQuantidade(Produto)
+                return true;
+            }
+        });
+        return true;
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atualizar_estoque_dialog);
@@ -54,12 +71,16 @@ public class AtualizarEstoque extends AppCompatActivity
                 ImageButton buttonMinus = (ImageButton) v.findViewById(R.id.prod_icon);
                 ImageButton buttonPlus = (ImageButton) v.findViewById(R.id.prod_button);
 
+                /**
+                 * As modificações só serão salvas depois da confirmação
+                 */
                 buttonMinus.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         // É preciso fazer um check se isso realmente funciona
                         // Acredito que sim
                         model.setQuantidade(model.getQuantidade() - 1);
+
                     }
                 });
                 buttonPlus.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +92,10 @@ public class AtualizarEstoque extends AppCompatActivity
             }
         };
         listaProdutos.setAdapter(produtos);
+    }
+    public void updateQuantidade(Produto prod){
+        DatabaseReference ref = Produto.DBROOT;
+        ref.child(prod.getCodDBarras()).child("quantidade").setValue(prod.getQuantidade())
     }
 
 }
